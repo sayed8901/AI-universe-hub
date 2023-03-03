44054742cc00259cd70a78c34dffd52ac7283f78
sayed8901 <sayed91515@gmail.com>
2023-03-03 19:33:50 +0600

    sort by date part completed

--- ai.js
+++ ai.js
@@ -1,29 +1,16 @@
-document.getElementById('sort-result-by-latest-date').addEventListener('click', function(){
-    // fetching the ai universe url to get data from it.
-    const loadData = async(dataLimit) => {
-        const url = `https://openapi.programming-hero.com/api/ai/tools`;
-        try{
-            const res = await fetch(url);
-            const data = await res.json();
-            displayData(data.data.tools, dataLimit);
-        }
-        catch(error){
-            console.log(error);
-        }
+// fetching the ai universe url to get data from it.
+const loadData = async(dataLimit) => {
+    const url = `https://openapi.programming-hero.com/api/ai/tools`;
+    try{
+        const res = await fetch(url);
+        const data = await res.json();
+        displayData(data.data.tools, dataLimit);
     }
+    catch(error){
+        console.log(error);
+    }
+}
 
-    // to set how many cards want to display at first load
-    loadData(2);
-    // spinner starts
-    toggleSpinner(true);
-
-    // to show all by clicking on 'see more' button
-    document.getElementById('btn-show-all').addEventListener('click', function(){
-        loadData();
-        // spinner starts
-        toggleSpinner(true);
-    })
-})
 
 // display info in the cards
 const displayData = (data, dataLimit) => {    
@@ -79,6 +66,7 @@ const displayData = (data, dataLimit) => {
     });
 }
 
+
 // spinner loading function
 const toggleSpinner = isLoading => {
     const spinner = document.getElementById('spinner');
@@ -89,3 +77,67 @@ const toggleSpinner = isLoading => {
         spinner.classList.add('d-none');
     }
 }
+
+
+// to set how many cards want to display at first load
+loadData(6);
+// spinner starts
+toggleSpinner(true);
+
+// to show all by clicking on 'see more' button
+document.getElementById('btn-show-all').addEventListener('click', function(){
+    loadData();
+    // spinner starts
+    toggleSpinner(true);
+})
+
+
+
+
+
+
+
+// sort by date part
+document.getElementById('sort-result-by-latest-date').addEventListener('click', function(dataLimit){
+    // fetching the ai universe url to get data from it.
+    const loadData = async(dataLimit) => {
+        const url = `https://openapi.programming-hero.com/api/ai/tools`;
+        try{
+            const res = await fetch(url);
+            const data = await res.json();
+            sort(data.data.tools, dataLimit);
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+
+    // function to sort the objects on an array according to the latest date
+    customSort = (a,b) => {
+        const dateA = new Date(a.published_in);
+        const dateB = new Date(b.published_in);
+        if(dateA < dateB) return 1;
+        else if(dateA > dateB) return -1;
+        else return 0;
+    }
+
+    // soring the original url array using the above mentioned function
+    const sort = (url, dataLimit) => {
+        const sortedUrl = url.sort(customSort);
+        console.log(sortedUrl);
+        displayData(sortedUrl, dataLimit);
+    }
+
+
+    // to set how many cards want to display at first load
+    loadData(6);
+    // spinner starts
+    toggleSpinner(true);
+
+    // to show all by clicking on 'see more' button
+    document.getElementById('btn-show-all').addEventListener('click', function(){
+        loadData();
+        // spinner starts
+        toggleSpinner(true);
+    })
+})
